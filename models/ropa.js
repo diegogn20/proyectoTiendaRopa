7ba7f6cc21b4064b49d6a4e3ce8fdcf057436f40
diego <diegogeinerns@gmail.com>
2024-10-15 23:08:31 -0300

    el server corre bien

--- models/ropa.js
+++ models/ropa.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 
-const ropaSchema = mongoose.Schema({
-  id:{type: Number, required: true}, 
-  nombre: { type: String, required: true },
+const ropaSchema = new mongoose.Schema({
+  nombre: { type: String, required: true, trim: true },
   categoria: { type: String, enum: ['dama', 'caballero', 'unisex'], required: true },
-  color: { type: String, required: true },
+  color: { type: String, required: true,type: String,required: true, match: /^#([0-9A-F]{3}){1,2}$/i },
   precio: { type: Number, required: true },
   talle: { type: String, required: true },
   stock: { type: Number, default: 0 }
-});
+}, {collection: 'ropa'}); 
 
 const Ropa = mongoose.model('Ropa', ropaSchema);
 
--- server.js
+++ server.js
@@ -1,44 +1,45 @@
-const express = require('express');
-const ropaRoutes = require('./routes/ropa');
-const mongoose = require('mongoose');
+const express = require("express");
+const ropaRoutes = require("./routes/ropa");
+const mongoose = require("mongoose");
 
 class Server {
+  constructor() {
+    this.port = process.env.PORT || 3000;
+    this.app = express();
 
-    constructor() {
-        this.port = process.env.PORT || 3000;
-        this.app = express();
+    this.conectarBaseDeDatos();
 
-        this.conectarBaseDeDatos();
+    //cargar middlewares
+    this.cargarMiddlewares();
 
-        //cargar middlewares
-        this.cargarMiddlewares();
+    //definir rutas
+    this.cargarRutas();
+  }
 
-        //definir rutas
-        this.cargarRutas();
+  async conectarBaseDeDatos() {
+    try {
+      // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+      await mongoose.connect(process.env.MONGODB_URI);
+      console.log("Conectado a BD en la nube!");
+    } catch (e) {
+      console.log("Error al conectar BD en la nube!");
+      console.log(e);
     }
+  }
 
-    conectarBaseDeDatos() {
-        // Conectar a MongoDB
-        mongoose.connect(process.env.MONGODB_URI)
-        .then(() => {})
-        .catch((error) => {
-            console.log(error) 
-        })
-    }
+  cargarMiddlewares() {
+    this.app.use(express.json()); // Habilitar parsing de JSON
+  }
 
-    cargarMiddlewares() {
-        this.app.use(express.json()); // Habilitar parsing de JSON
-    }
+  cargarRutas() {//http://localhost:3000/api/ropa
+    this.app.use("/api/ropa", ropaRoutes); //Rutas de la API de ropa
+  }
 
-    cargarRutas() {
-        this.app.use('/api/ropa', ropaRoutes); // Rutas de la API de ropa
-    }
-
-    listen() {
-        this.app.listen(this.port, () => {
-            console.log(`Servidor corriendo en http://localhost:${this.port}`);
-        });
-    }
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Servidor corriendo en http://localhost:${this.port}`);
+    });
+  }
 }
 
 module.exports = Server;
