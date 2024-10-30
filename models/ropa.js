d3c15120ac975eca555f751376cddd29756fd90d
diego <diegogeinerns@gmail.com>
2024-10-30 19:48:04 -0300

    agregado pedido y usuario

--- models/ropa.js
+++ models/ropa.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const ropaSchema = new mongoose.Schema({
     nombre: {type: String, required: true, trim: true},
     categoria: {type: String, enum: ['dama','caballero','unisex'], required: true},
+    marca: {type: Number, required: true},
     precio: {type: Number, required: true},
     talle: {type: String, required: true},
     stock: {type: Number, default: 0},
